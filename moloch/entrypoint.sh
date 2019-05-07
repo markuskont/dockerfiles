5163193dc029ee8d95f6ddf6be75bbbf5fdeb86f
Markus Kont <markus.kont@ccdcoe.org>
2019-05-07 16:40:35 +0300

    introducing moloch. Entrypoint and core image works, not finalized

---
+++ moloch/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+CONFIG=/data/moloch/etc/config.ini
+ifaces="enp0s3"
+cp $CONFIG.sample $CONFIG
+
+sed -i "s/MOLOCH_INSTALL_DIR/\/data\/moloch/g"    $CONFIG
+
+if [[ -z "${MOLOCH_ELASTICSEARCH}" ]]; then
+  MOLOCH_ELASTICSEARCH="localhost:9200"
+fi
+if [[ -z "${MOLOCH_INTERFACE}" ]]; then
+  MOLOCH_INTERFACE="eth0"
+fi
+if [[ -z "${MOLOCH_PASSWORD}" ]]; then
+  MOLOCH_PASSWORD="D0ker1z3dd#"
+fi
+
+sed -i "s/MOLOCH_ELASTICSEARCH/${MOLOCH_ELASTICSEARCH}/g" $CONFIG
+sed -i "s/MOLOCH_INTERFACE/${MOLOCH_INTERFACE}/g"         $CONFIG
+sed -i "s/MOLOCH_PASSWORD/${MOLOCH_PASSWORD}/g"           $CONFIG
+
+case ${MOLOCH_ENV} in
+  "CAPTURE" )
+    MOLOCH_ELASTICSEARCH=$(echo ${MOLOCH_ELASTICSEARCH} | cut -d "," -f1)
+    curl -ss $MOLOCH_ELASTICSEARCH || echo "unable to connect to elastic ${MOLOCH_ELASTICSEARCH}" ; exit 1
+    if [[ `./db/db.pl ${MOLOCH_ELASTICSEARCH} info | grep "DB Version" | cut -d ":" -f2 | tr -d " "` -eq -1 ]]; then
+      echo "INIT" | ./db/db.pl ${MOLOCH_ELASTICSEARCH} init
+    fi
+    moloch-capture -c $CONFIG --help
+    ;;
+  *)
+    echo "MOLOCH_ENV undefined"
+esac
